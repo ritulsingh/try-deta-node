9c1c6a0881afee84887716840300aefc566273dd
Ritul Singh <ritul.mtalkz@gmail.com>
2023-02-03 12:33:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,20 +5,23 @@ const PORT = process.env.PORT || 3000;
 
 const deta = Deta('d07ituoq_jxynmVrMockamon1DigraQFBdb9E7t1i');
 
-
 // set the database name
 const db = deta.Base('simpleDB');
 
-// Declare a route
-fastify.get('/', async (request, res) => {
-    res.send("Ritul Singh");
-})
 // Get all Data from database
-fastify.get('/getdata', async (req, res) => {
+fastify.get('/', async (request, res) => {
     const item = await db.fetch();
     res.send(item)
 })
 
+// Get Data by mobile number
+fastify.get('/email/:email', async (req, res) => {
+    const { email } = req.params;
+    const items = await db.fetch({ 'email': email });
+    res.send(items);
+})
+
+// Send data to database
 fastify.post('/send', async (req, res) => {
     try {
         const { name, mobileNumber, email } = req.body;
@@ -35,8 +38,13 @@ fastify.post('/send', async (req, res) => {
     } catch (err) {
         res.status(500).send(err);
     }
-
 })
+
+fastify.delete('/users/:id', async (req, res) => {
+    const { id } = req.params;
+    await db.delete(id);
+    res.send({"message": "deleted"})
+});
 
 // Start Local server listening for requests
 fastify.listen({ port: PORT, host: '127.0.0.1' }, function (err, address) {
